4e8121519289318bee9269cdbdbd21650e3ead9d
Ian Cha <chaheein@gmail.com>
2021-02-05 14:28:14 -0800

    department

--- src/main/webapp/src/app/app-routing.module.ts
+++ src/main/webapp/src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
+import { DepartmentListComponent } from './department-list/department-list.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {path: 'departments', component: DepartmentListComponent}
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [DepartmentListComponent]
