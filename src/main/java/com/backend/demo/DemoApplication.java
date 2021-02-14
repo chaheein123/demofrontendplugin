469e1755bc0196a6e449077af52980bc5fc8f74c
Ian Cha <chaheein@gmail.com>
2021-02-14 07:52:57 -0800

    testing

--- src/main/java/com/backend/demo/DemoApplication.java
+++ src/main/java/com/backend/demo/DemoApplication.java
@@ -2,6 +2,13 @@ package com.backend.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorViewResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Collections;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -9,5 +16,11 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
+//	@Bean
+//	public ErrorViewResolver customErrorViewResolver() {
+//		final ModelAndView redirectToIndexHtml = new ModelAndView("forward:/index.html", Collections.emptyMap(), HttpStatus.OK);
+//		return ((request, status, model) -> status == HttpStatus.NOT_FOUND ? redirectToIndexHtml : null);
+//	}
 
 }
---
+++ src/main/java/com/backend/demo/controller/Frontend.java
@@ -0,0 +1,32 @@
+package com.backend.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+//@RestController
+//@RequestMapping("/")
+//public class Frontend {
+//    @GetMapping("/")
+//
+//
+//}
+
+//@RestController
+//@RequestMapping("/api/productCategory")
+//public class ProductCategoryController {
+//
+//    @Autowired
+//    ProductService productService;
+//
+//    @GetMapping
+//    public List<String> getProductCategories(@RequestBody ProductCategoriesRequestDto request) {
+//        return productService.getProductCategoriesByRequest(request);
+//    }
+//
+//
+//}
